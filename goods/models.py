4abf95a58054375b258ce7d3f84a4591c320c2cf
matiiv <matiiv.r.a@gmail.com>
2022-06-21 20:05:48 +0300

    write models

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.db.models import CharField
+from django.db.models import FloatField
+from django.db.models import UUIDField
+from django.db.models import ForeignKey
+from django.db.models import DateTimeField
+
+
+class ShopUnitType(models.TextChoices):
+    OFFER = 'OFFER'
+    CATEGORY = 'CATEGORY'
+
+
+class ShopUnit(models.Model):
+    id = UUIDField(primary_key=True, editable=False)
+    name = CharField(max_length=255)
+    parent_id = ForeignKey('ShopUnit', on_delete=models.CASCADE, null=True, blank=True)
+    price = FloatField(null=True, blank=True,validators=[MinValueValidator(0)])   # todo для товаров не может быть NULL
+    type = CharField(max_length=20, choices=ShopUnitType.choices)
+    date = DateTimeField()
