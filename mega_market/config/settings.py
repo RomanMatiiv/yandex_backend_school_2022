3e8cc8b0accc538b899e65fd543cfed6512191cc
matiiv <matiiv.r.a@gmail.com>
2022-06-26 20:03:15 +0300

    move hardcode to env variable

--- mega_market/config/settings.py
+++ mega_market/config/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import environ
 import yaml
+
+env = environ.Env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,16 +25,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_^23wr_d!2mmgo42p111m(k3%g4g_#z52&m)2!gqdvd*g^_#78'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'external-2018.usr.yandex-academy.ru']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
-INTERNAL_IPS = ['127.0.0.1', 'localhost']
+INTERNAL_IPS = env.list('INTERNAL_IPS')
 
 # Application definition
 
@@ -81,10 +88,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db" /'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db" /'db.sqlite3',
+        'ENGINE': env.str('DB_ENGINE'),
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.int('DB_PORT'),
+        'OPTIONS': {
+           'options': '-c search_path=public,content'
+        }
     }
 }
 
